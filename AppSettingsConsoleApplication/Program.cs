c8142cc366db8e7a5b9603c57a4b1bbb968454b9
vishal <vishal@VISHAL-PC>
2018-09-29 14:00:04 +0530

    Program to read config from json and bind to object

---
+++ AppSettingsConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Sample
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot _IConfig = config.Build();
+            
+            var sample = new Sample();
+            _IConfig.GetSection("Sample").Bind(sample);
+            Sample _sObj = sample;
+            Console.WriteLine("The ID {0} and Name is {1}",_sObj.ID,_sObj.Name);
+        }
+    }
+}
